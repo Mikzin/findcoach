0dba419f50babee542c22ec431a86fd8cbcd8763
Mikhail Zinchuk <viper92@inbox.ru>
2022-10-24 21:01:14 +0200

    feat: add files&routing

---
+++ src/router.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestRecieved from './pages/requests/RequestRecieved.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
+      path: '/coaches',
+      component: CoachesList,
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [
+        {
+          path: '/contact',
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+    },
+    {
+      path: '/requests',
+      component: RequestRecieved,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
+  history: createWebHistory(),
+});
+
+export default router;
