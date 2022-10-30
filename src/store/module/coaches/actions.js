f334fab353783e0b12b4727748f73d63c8b8c005
Mikhail Zinchuk <viper92@inbox.ru>
2022-10-30 22:14:20 +0100

    feat: add database and fetches

--- src/store/module/coaches/actions.js
+++ src/store/module/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,45 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://findcoach-3a683-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    //const responseData = await response.json();
+
+    if (!response.ok) {
+      //error...
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://findcoach-3a683-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const resoponseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(resoponseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in resoponseData) {
+      const coach = {
+        firstName: resoponseData[key].firstName,
+        lastName: resoponseData[key].lastName,
+        description: resoponseData[key].description,
+        hourlyRate: resoponseData[key].hourlyRate,
+        areas: resoponseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   },
 };
